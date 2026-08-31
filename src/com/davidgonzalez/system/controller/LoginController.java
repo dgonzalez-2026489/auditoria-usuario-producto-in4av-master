5904a41efade0c65719edb197e7615337ccb9c04
David Gonzalez <dgonzalez-2026489@kinal.edu.gt>
2026-08-31 11:31:52 -0600

    feat: arreglo de comprobaciones, logica e implementación del dashboard

---
+++ src/com/davidgonzalez/system/controller/LoginController.java
@@ -0,0 +1,45 @@
+package com.davidgonzalez.system.controller;
+
+import com.davidgonzalez.system.utils.ViewFactory;
+import com.davidgonzalez.system.utils.AlertInformation;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController implements Initializable{
+    private final ViewFactory viewFactory = new ViewFactory();
+    private AlertInformation alerta = new AlertInformation();
+    
+    @FXML
+    private TextField txtUsuario;
+    
+    @FXML
+    private Hyperlink lnkRegistro;
+    
+    @FXML
+    private void onRegister(MouseEvent event) {
+        ViewFactory viewFacto = new ViewFactory();
+        viewFacto.viewRegistro();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+    @FXML
+    public void validarLogin(ActionEvent evento) {
+        if(txtUsuario.getText().isEmpty()) {
+            txtUsuario.getStyleClass().add("error-campo");
+            alerta.mostrarAlerta("error", "No sabes llenar un campo", "Error de campos vacios");
+        } else {
+            txtUsuario.getStyleClass().remove("error-campo");
+            alerta.mostrarAlerta("confirm", "Nitido", "Eres exitoso");
+        }
+    }
+}
---
+++ src/com/davidgonzalez/system/repository/UserInterface.java
@@ -0,0 +1,7 @@
+package com.davidgonzalez.system.repository;
+
+import com.davidgonzalez.system.model.Users;
+
+public interface UserInterface {
+    void create(Users user);
+}
